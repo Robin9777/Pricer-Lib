cfff304ad6ac8a2b6c6d2c5f5837ee81e469004e
Robin9777 <rbouilliol@gmail.com>
2026-04-15 19:09:12 +0200

    VarRed: Creating Variance Reduction project

---
+++ VarRed/VarRed.cpp
@@ -0,0 +1,10 @@
+// VarRed.cpp : Définit les fonctions de la bibliothèque statique.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Il s'agit d'un exemple de fonction de bibliothèque
+void fnVarRed()
+{
+}

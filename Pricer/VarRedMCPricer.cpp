3447ebe75f278aca5c05669cacea74dc4b926d3c
Robin9777 <rbouilliol@gmail.com>
2026-05-02 13:56:10 +0200

    VarRed & UnitTestVarRed: implementation of static control variate for Basket Options

---
+++ Pricer/VarRedMCPricer.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include "VarRedMCPricer.h"
+#include <cmath>
+
+
+VarRedMCPricer::VarRedMCPricer(RandomProcess* _process, PayOff* _payoff, ControlVariate* _cv, double _rate, double _maturity, size_t _nbSim, size_t _nbSteps) :
+	MCPricer(_process, _payoff, _rate, _maturity, _nbSim, _nbSteps), CV(_cv)
+{
+}
+
+PriceResult VarRedMCPricer::Price()
+{
+	double totalPayoff = 0.0;
+	double squaredPayoff = 0.0;
+
+	double cvExpectation = CV->AnalyticalExpectation();
+
+	for (size_t sim = 0; sim < nbSim; ++sim) {
+		
+		//classic approach
+		Process->Simulate(0.0, maturity, nbSteps);
+		std::vector<SinglePath*> paths;
+		for (int d = 0; d < Process->GetDimension(); ++d)
+			paths.push_back(Process->GetPath(d));
+
+		// Static control variate
+		double cvValue = CV->SimulatedValue(paths);
+
+		double discountedPayoff = std::exp(-rate * maturity) * (*Payoff)(paths);
+		double adjustedPayoff = discountedPayoff - cvValue + cvExpectation;
+
+		totalPayoff += adjustedPayoff;
+		squaredPayoff += adjustedPayoff * adjustedPayoff;
+	}
+
+	double averagePrice = totalPayoff / static_cast<double>(nbSim);
+	double variance = (squaredPayoff / static_cast<double>(nbSim)) - (averagePrice * averagePrice);
+
+	return {averagePrice, 1.96 * std::sqrt(variance / static_cast<double>(nbSim))};
+}
--- VarRed/BasketGeomControlVariate.h
+++ VarRed/BasketGeomControlVariate.h
@@ -8,8 +8,20 @@ private:
 	std::vector<double> Spots;
 	std::vector<std::vector<double>> correlMatrix;
 	std::vector<double> Vols;
+	double maturity;
+	double strike;
+	double rate;
 
 public:
+	BasketGeomControlVariate(
+		const std::vector<double>& _weights,
+		const std::vector<double>& _spots,
+		const std::vector<std::vector<double>>& _correlMatrix,
+		const std::vector<double>& _vols,
+		double _maturity,
+		double _strike,
+		double _rate
+	);
 
 	double SimulatedValue(const std::vector<SinglePath*>& paths) const override;
 
